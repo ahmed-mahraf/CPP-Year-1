71d7446cfa2b0f536137cb84b85bf54f1e5cd0b1
Mahraf Ahmed <ahmed.mahraf@outlook.com>
2020-12-14 13:34:15 +0000

    version 1.0 - Program can enter string but with no spaces

--- StringsManipulation/main.cpp
+++ StringsManipulation/main.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
+#include <string>
+#include <algorithm> // access std::replace_if
+
+std::string stringToText, textResult;
 
 int main() {
+	std::cout << "Enter a string:" << std::endl;
+
+	std::cin >> stringToText;
+
+	std::replace_if(
+		stringToText.begin(),
+		stringToText.end(),
+		std::ispunct, 
+		' ');
+
+	std::cout << stringToText << std::endl;
+
+	std::system("pause");
 	return 0;
 }
